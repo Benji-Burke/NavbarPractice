cc9e7d18036be083f7b07a4d665af1a5de56b521
Benjamin Burke <benjaminstevenburke@gmail.com>
2020-12-05 09:49:59 -0500

    updated the service

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,10 @@ const About = () => {
             <h1>What is anime fund?</h1>
             <p>Anime fund is a membership platform where you as the creator can 
                 bring content to your patreons on a monthly basis.</p>
+                <p>Or
+                    <br/>
+                    Get help funding a potential anime. The possibilities are endless!
+                </p>
         </div>
     )
 }
