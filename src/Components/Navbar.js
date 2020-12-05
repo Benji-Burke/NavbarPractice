e179c381938ac98024e7a143555524431b55898c
Ben <benjaminstevenburke@gmail.com>
2020-12-05 06:35:08 -0500

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Styles.css';
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                <a className="logo">Brand</a>
+                <div className="nav-items">
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                </div>
+                <div>
+                    <input/>
+                    <a>login</a>
+                    <button>Create on Anime.Fund</button>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
